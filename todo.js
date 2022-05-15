2dbcccd1fc815fb4fe933ddfeea04cb87c5725a9
Mesut Asci <mesutasci@Mesuts-MacBook-Pro.local>
2022-05-15 11:37:44 +0200

    Removing ToDos

--- todo.js
+++ todo.js
@@ -3,6 +3,11 @@ const todoDate = document.querySelector("#todo-date");
 let table = document.querySelector(".table");
 let tBody = document.querySelector(".table-body");
 
+// Data structure
+let todoList = [
+];
+
+// Forming table rows
 function formRows(pList) {
     return tBody.innerHTML = pList
         .map((todo, index) => {
@@ -11,7 +16,7 @@ function formRows(pList) {
                 <th scope="row" class="col-num">${index + 1}</th>
                 <td class="col-todo">${todo.nameTodo}</td>
                 <td class="col-date">${todo.dateTodo}</td>
-                <td class="delete"><i class="fa-solid fa-trash-can"></i></td>
+                <td class="delete"><i class="fa-solid fa-trash-can" onclick="deleteTodo(${index})"></i></td>
             </tr>
             `
         }
@@ -19,19 +24,12 @@ function formRows(pList) {
         ).join("")
 }
 
-// Add new todos to the to the list
-let todoList = [
-];
-
-// if (todoList.length !== 0) {
-//     table.innerHTML = `
-//     <h2>...Your ToDo list is empty....</>`
-// }
-
+// Adding a todo to the list
 const addTodo = function () {
     const nameTodo = todoName.value.trim(); // moving white spaces
     const dateTodo = todoDate.value; // moving white spaces
     const newTodo = {
+        // id: todoList.length, // id of the each list objects
         nameTodo: nameTodo,
         dateTodo: dateTodo
     }
@@ -43,108 +41,13 @@ const addTodo = function () {
     formRows(todoList);
 }
 
-
-
-
-// formRows(todoList);
-
-// // Adding rows to the table
-// function addRows(Array) {
-//     let owlCarousel = document.querySelector('.owl-carousel');
-//     owlCarousel.innerHTML = addCards(Array);
-//     return owlCarousel;
-// }
-
-// render(todoList);
-
-
-
-
-// const addForm = document.querySelector(".add");
-// const list = document.querySelector("todos");
-// const addButton = document.querySelector(".btn");
-// let html = "";
-// // Add new list element to HTML
-// let generateTemplate = todo => {
-
-//     let Li = `
-//     <li
-//     class="
-//       list-group-item
-//       d-flex
-//       justify-content-between
-//       align-items-center
-//     "
-//   >
-//     <span>${todo}</span>
-//     <i class="far fa-trash-alt delete"></i>
-//   </li>
-//     `;
-
-//     html += Li;
-
-//     list.innerHTML =`
-//          <ul class="todos list-group mx-auto text-light">
-//              ${html}
-//          </ul>`
-// };
-
-// // Input new value to the list
-// addButton.addEventListener("click", e => {
-
-//     e.preventDefault();
-//     const todo = addForm.add.value;
-
-//     if(todo.length){
-//         generateTemplate(todo);
-//         addForm.reset();
-//     }
-
-// });
-
-// list.addEventListener("click", e => {
-//     if (    e.target.classList.contains('delete')){
-//         e.target.parentElement.remove();
-//     }});
-
-
-
-
-
-// const addButton = document.querySelector("#add-todo");
-// const todoList = document.querySelector("#todo-list");
-// addButton.addEventListener("click", newTodo);
-// const row =
-// const deleteRow = document.querySelector("#delete-row");
-// deleteRow.addEventListener("click", removeRow);
-
-// let html = "";
-
-// function newTodo(){
-//     todo = document.querySelector("#new-todo").value;
-//     if (todo !== ""){
-
-//         // priority setting
-//         let priority = "";
-
-//         let row = `
-//             <tr>
-//                 <td class="todo">${todo}</td>
-//                 <td class="priority"><span>high</span></td>
-//                 <td class="date">today</td>
-//                 <td><i class="fas fa-trash-alt"></i></td>
-//             </tr>
-//             `
-//         html += row;
-//         document.querySelector("#new-todo").value = "";
-
-//         todoList.innerHTML =`
-//         <tbody id="todo-list">
-//             ${html}
-//         </tbody>
-//         `
-//     }
-//     else{
-//         alert("Please type in the box something to do");
-//     }
-// }
+// Removing ToDos from the list
+function deleteTodo(index) {
+    let selectedId = index;
+    console.log("deleted todo:", selectedId);
+    console.log(todoList);
+    let filteredList = todoList.filter((todo, index) => todoList.indexOf(todo) !== selectedId);
+    todoList = filteredList;
+    console.log(todoList);
+    formRows(todoList)
+}
