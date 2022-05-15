a7669446407b3e9b62d7f180eef6ffe28a2e95df
Mesut Asci <mesutasci.76@gmail.com>
2022-05-15 13:12:49 +0200

    sweetalert2 added: alert, confirmation, success

--- todo.js
+++ todo.js
@@ -35,8 +35,25 @@ const addTodo = function () {
     }
     if (nameTodo !== "" && dateTodo !== "") {
         todoList.push(newTodo); // preventing to input empty values
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your ToDo has been added!',
+            showConfirmButton: false,
+            timer: 1200
+        })
     }
+    else {
+        // alert("Please do not forget to enter a todo or the deadline!")
+        Swal.fire(
+            'Alls filled?',
+            'Please do\'nt forget to enter a TODO and a DEADLINE!',
+            'question'
+        )
+    }
+
     formRows(todoList);
+
     // clearing input values
     todoName.value = "";
     todoDate.value = "";
@@ -45,10 +62,24 @@ const addTodo = function () {
 // Removing ToDos from the list
 function deleteTodo(index) {
     let selectedId = index;
-    console.log("deleted todo:", selectedId);
-    console.log(todoList);
-    let filteredList = todoList.filter((todo, index) => todoList.indexOf(todo) !== selectedId);
-    todoList = filteredList;
-    console.log(todoList);
-    formRows(todoList)
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert your ToDo!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let filteredList = todoList.filter((todo, index) => todoList.indexOf(todo) !== selectedId);
+            todoList = filteredList;
+            formRows(todoList)
+            Swal.fire(
+                'Deleted!',
+                'Your ToDo has been deleted.',
+                'success'
+            )
+        }
+    })
 }
